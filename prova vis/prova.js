3c1bd47f1533056a8c7a52867c16e199687a824b
Rei Beshiri <rei.beshiri@studio.unibo.it>
2019-03-18 10:53:13 +0100

    added counter persone

--- prova vis/prova.js
+++ prova vis/prova.js
@@ -1,14 +1,52 @@
 var chart;
+var max_posti_in_biblioteca = 123; //max posti = 2499 -> 24 + 99 = 123
 $(document).ready(function() {
+
+  //inizio counter
+   var counter_list = [00,00000,"persone: "];
+   var str_counter_0 = counter_list[0];
+   var str_counter_1 = counter_list[1];
+   var str_counter_2 = counter_list[2];
+   var display_str = "";
+   var display_div = document.getElementById("counter_id");
+
+   setInterval(function(){
+     // clear count
+     while (display_div.hasChildNodes()) {
+         display_div.removeChild(display_div.lastChild);
+     }
+     if(str_counter_1 + str_counter_0 < max_posti_in_biblioteca){
+       str_counter_0++;
+     }
+     if (str_counter_0 > 99) {
+       str_counter_0 = 10; // reset count
+       str_counter_1++;    // increase next count
+     }
+     if(str_counter_1>99999){
+       str_counter_2++;
+     }
+     display_str = str_counter_2.toString() + str_counter_1.toString() + str_counter_0.toString();
+     for (var i = 0; i < display_str.length; i++) {
+       var new_span = document.createElement('span');
+       new_span.className = 'num_tiles';
+       new_span.innerText = display_str[i];
+       display_div.appendChild(new_span);
+     }
+   },10);
+   ///////////////fine counter_lis
+
   graph("myChart", "bar", 213, 129, 159, 200);
   /*graph("myChart2", "pie", 113, 229, 259, 100);
   graph("myChart3", "line", 113, 229, 259, 100);*/
+
+  /*
   console.log($(document).width());
   console.log($(window).width());
-  console.log(screen.width); // QUESTO è MEGLIO
+  console.log(screen.width); //QUESTO è MEGLIO
   console.log($(document).height());
   console.log($(window).height());
-  console.log(screen.height);
+  console.log(screen.height); //QUESTO è MEGLIO
+  */
 
   /*var run=new ActiveXObject('WSCRIPT.Shell').Run("type NUL > 1.txt");
   /*var shell = WScript.CreateObject("WScript.Shell");
@@ -23,7 +61,7 @@ setInterval(function(){
     console.log(Math.round(l1));
     var data = l1;
     addData(chart, "new data", data);
-  }, 2000);
+  }, 10000);
 });
 
 //crea il grafico a partire da id nel html, tipo grafico e valori
