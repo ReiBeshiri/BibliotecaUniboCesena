64c851b08c08801200f0ae82ea36d7dc236cc65b
Rei Beshiri <rei.beshiri@studio.unibo.it>
2019-03-20 17:10:54 +0100

    fixed script for login liber8portal

---
+++ utils/php-py-mysql/script-login-liber8portal.py
@@ -0,0 +1,22 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome() #get the Chrome tool (ChromeDriver, FirefoxDriver, SafariDriver most used)
+driver.maximize_window()
+driver.get('https://www.liber8portal.com')
+#try to iterate through elements starting from a tag name, throws exeptions
+#elements = driver.find_elements_by_tag_name('a')
+#for element in elements:
+#    if element.text == 'Login':
+#        element.click()
+time.sleep(3)
+#driver.find_element_by_css_selector('div p a').click()
+username = driver.find_element_by_id("Username")
+password = driver.find_element_by_id("Password")
+username.send_keys("provausername")
+password.send_keys("provapassword")
+password.send_keys(Keys.ENTER)
+time.sleep(5)
+driver.quit()
