1cfeb6565b4fbdf56f81381bc5b6b6cbb9287974
Rei Beshiri <rei.beshiri@studio.unibo.it>
2019-04-01 12:41:48 +0200

    modificata visualizzazione grafici

--- prova vis/prova.js
+++ prova vis/prova.js
@@ -1,8 +1,7 @@
-var chart1;
-var chart2;
-var chart3;
-var chart4;
-var chart5;
+var chart1; //day
+var chart2; //week
+var chart3; //month
+var chart4; //year
 var chart;
 var max_posti_in_biblioteca = 123; //max posti = 2500 -> 2499 => 24 + 99 = 123
 $(document).ready(function() {
@@ -15,6 +14,7 @@ $(document).ready(function() {
    var display_str = "";
    var display_div = document.getElementById("counter_id");
 
+   //funzione che modifica il contatore
    setInterval(function(){
      // clear count
      while (display_div.hasChildNodes()) {
@@ -41,87 +41,229 @@ $(document).ready(function() {
    //fine counter_lis
 
   //id html chart, tipo di chart, n valori (lun,mar,mer,gio,ven oppure altri poi decidi)
-  graph("myChart", "bar", 2130, 1290, 1590, 2000);
+  graphDay("myChart", "line", 830, 1290, 1590, 2000);
   chart1 = chart;
-  graph("myChart2", "pie", 1130, 2290, 2490, 1000);
+  graphWeek("myChart2", "bar", 1130, 2290, 2490, 1000);
   chart2 = chart;
-  graph("myChart3", "line", 1600, 2100, 2490, 981);
+  graphMonth("myChart3", "bar", 1600, 2100, 2490, 981);
   chart3 = chart;
-  graph("myChart4", "bar", 1430, 1990, 2190, 1260);
+  graphYear("myChart4", "bar", 1430, 1990, 2190, 1260);
   chart4 = chart;
-  graph("myChart5", "line", 1530, 1800, 2590, 1110);
-  chart5 = chart;
 
   /*
   console.log(screen.width);
   console.log(screen.height);
   */
 
-//code before the pause
-setInterval(function(){
-    var l1 = Math.random()*2500;
-    console.log(Math.round(l1));
-    var data = l1;
-    addData(chart1, "new", data);
-  }, 5000);
-});
-
-//crea il grafico a partire da id nel html, tipo grafico e valori
-function graph(id,type,a,b,c,d) {
-  var l=a;
-  var m=b;
-  var g=c;
-  var v=d;
-  var ctx = document.getElementById(id).getContext('2d');
-  var myChart = new Chart(ctx, {
-      type: type,
-      data: {
-          labels: ["Lun", "Mar", "Mer", "Gio", "Ven"],
-          datasets: [{
-              label: '# of people',
-              data: [l, m, m, g, v],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.4)',
-                  'rgba(54, 162, 235, 0.4)',
-                  'rgba(255, 206, 86, 0.4)',
-                  'rgba(75, 192, 192, 0.4)',
-                  'rgba(153, 102, 255, 0.4)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-
-      "responsive": true,
-      "maintainAspectRatio": false,
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
-      }
+  //code before the pause
+  setInterval(function(){
+      var l1 = Math.random()*2500;
+      console.log(Math.round(l1));
+      var data = l1;
+      addData(chart1, "new", data);
+    }, 50000000);
   });
-  chart = myChart;
-}
 
-function addData(chart, label, data) {
-var i = 0;
-chart.data.labels.push(label);
-chart.data.datasets.forEach((dataset) => {
-  dataset.data.push(data);
-  i++
-});
-//console.log(chart.data.datasets[0].data[n]); //prendi la colonna n (a partire da desrtra).
-//chart.data.datasets[0].backgroundColor[n];   //prendi l'array della colonna n che indica il colore in background della colonna.
-//chart.data.datasets[0].borderColor[n] //prendi l'array della colonna n che indica il colore del bordo della colonna.
-chart.update();
-}
+  //funzione per costruire il primo grafico (quello giornaliero)
+  function graphDay(id,type,a,b,c,d) {
+    var l=a;
+    var m=b;
+    var g=c;
+    var v=d;
+    var ctx = document.getElementById(id).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: type,
+        data: {
+            labels: ["7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"],
+            datasets: [{
+                label: 'andamento giornaliero',
+                data: [l, m, m, g, v, l, m, m, g, v, g],
+                backgroundColor: [
+                    'rgba(55, 99, 132, 0.4)'
+                ],
+                borderColor: [
+                    'rgba(55, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+
+        "responsive": true,
+        "maintainAspectRatio": false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    chart = myChart;
+  }
+  //funzione per costruire il primo grafico (quello annuale)
+  function graphYear(id,type,a,b,c,d) {
+    var l=a;
+    var m=b;
+    var g=c;
+    var v=d;
+    var ctx = document.getElementById(id).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: type,
+        data: {
+            labels: ["gen", "feb", "mar", "apr", "mag", "giu", "lug", "ago", "set", "ott", "nov", "dic"],
+            datasets: [{
+                label: 'andamento annuale',
+                data: [l, m, m, g, v, l, m, m, g, v, g, l],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.4)',
+                    'rgba(54, 162, 235, 0.4)',
+                    'rgba(255, 206, 86, 0.4)',
+                    'rgba(75, 192, 192, 0.4)',
+                    'rgba(153, 102, 255, 0.4)',
+                    'rgba(255, 99, 132, 0.4)',
+                    'rgba(54, 162, 235, 0.4)',
+                    'rgba(255, 206, 86, 0.4)',
+                    'rgba(75, 192, 192, 0.4)',
+                    'rgba(153, 102, 255, 0.4)',
+                    'rgba(255, 99, 132, 0.4)',
+                    'rgba(54, 162, 235, 0.4)',
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+
+        "responsive": true,
+        "maintainAspectRatio": false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    chart = myChart;
+  }
+  //funzione per costruire il primo grafico (quello settimanale)
+  function graphWeek(id,type,a,b,c,d) {
+    var l=a;
+    var m=b;
+    var g=c;
+    var v=d;
+    var ctx = document.getElementById(id).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: type,
+        data: {
+            labels: ["Lun", "Mar", "Mer", "Gio", "Ven"],
+            datasets: [{
+                label: 'andameno settimanale',
+                data: [l, m, m, g, v],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.4)',
+                    'rgba(54, 162, 235, 0.4)',
+                    'rgba(255, 206, 86, 0.4)',
+                    'rgba(75, 192, 192, 0.4)',
+                    'rgba(153, 102, 255, 0.4)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+
+        "responsive": true,
+        "maintainAspectRatio": false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    chart = myChart;
+  }
+  //funzione per costruire il primo grafico (quello mensile)
+  function graphMonth(id,type,a,b,c,d) {
+    var l=a;
+    var m=b;
+    var g=c;
+    var v=d;
+    var ctx = document.getElementById(id).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: type,
+        data: {
+            labels: ["sett1", "sett2", "sett3", "sett4", "sett5"],
+            datasets: [{
+                label: 'andameno mensile',
+                data: [l, m, m, g, v],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.4)',
+                    'rgba(54, 162, 235, 0.4)',
+                    'rgba(255, 206, 86, 0.4)',
+                    'rgba(75, 192, 192, 0.4)',
+                    'rgba(153, 102, 255, 0.4)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+
+        "responsive": true,
+        "maintainAspectRatio": false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    chart = myChart;
+  }
+  //dato un chart modifico i dati
+  function addData(chart, label, data) {
+  var i = 0;
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(data);
+    i++
+  });
+  //console.log(chart.data.datasets[0].data[n]); //prendi la colonna n (a partire da desrtra).
+  //chart.data.datasets[0].backgroundColor[n];   //prendi l'array della colonna n che indica il colore in background della colonna.
+  //chart.data.datasets[0].borderColor[n] //prendi l'array della colonna n che indica il colore del bordo della colonna.
+  chart.update();
+  }
